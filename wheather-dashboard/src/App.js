c30eea292c4738f3b24672c33b3d54ff106bf91b
Hhaha <hariwong.w@gmail.com>
2025-03-07 10:37:27 +0700

    w8-create Heading
    sheet7.p6-Functional Components (props)

--- wheather-dashboard/src/App.js
+++ wheather-dashboard/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
+// import Heading from "./components/Heading";
+import Heading2 from "./components/Heading2";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <div className="main-content">
-        <h1>Dashboard</h1>
+        <Heading2 title="Dashboard" subtitle="Welcome to my dashboard" />
         <Dashboard />
       </div>
     </div>
