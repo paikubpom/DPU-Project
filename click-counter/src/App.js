8e8d72ff256edb9e1311916bbd90917a84286269
Hhaha <hariwong.w@gmail.com>
2025-03-28 11:33:08 +0700

    create Countdown

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./components/Counter";
+import Clock from "./components/Clock";
+import Countdown from "./components/Countdown";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Counter />
-    </>
+      <Clock />
+      <Countdown/>
+    </div>
   );
 }
 
